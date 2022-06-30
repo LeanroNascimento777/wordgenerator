eb63d6ae042d7723b0dc8ad766394f745845e416
Leandro Nascimento <concursos.publicos.lthn7@gmail.com>
2022-06-30 00:13:19 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:html';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//criando classe MyApp quer erda um Statelees
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  //iniciando o construtor build
+  Widget build(BuildContext context) {
+    //este método espera o retorno de M.A
+    return MaterialApp(
+      // O M.A tem varios gadgets usaremos o Title
+      title: "Startup Name Ganerator",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name generator'),
+          backgroundColor: Colors.green,
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ), // Scaffold é uma tela
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+  }
+}
